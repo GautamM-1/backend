ee82d36eb2a214e089d24a06b93e0c3cd1505ef2
Gautam Maity <gautammaity2004@gmail.com>
2026-08-14 02:26:34 +0530

    set the structure of res and err

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import app from "./app.js";
 import connectDB from "./db/db.js";
 import dotenv from "dotenv";
 
@@ -5,7 +6,15 @@ dotenv.config({
   path: "./.env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log("server is running");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 /*const app = express();
 (async () => {
